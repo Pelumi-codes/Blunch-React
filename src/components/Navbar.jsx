2baac22000a5913178171f12ed58d2ad578d7f31
Odinaka Ezennia <ogidi58@gmail.com>
2021-08-15 13:46:52 +0100

    add socials

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import Container from "./Container";
 import Logo from "./Logo";
+import instagram from "../assets/instagram.svg";
+import whatsapp from "../assets/whatsapp.svg";
 
 const Wrapper = styled(Container)`
   width: 100%;
@@ -12,6 +14,17 @@ const Wrapper = styled(Container)`
   height: 5.6rem;
   z-index: 5;
 
+  .socials {
+    width: max-content;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 2.4rem;
+
+    .icon {
+      height: 2.4rem;
+    }
+  }
+
   @media screen and (min-width: 768px) {
     height: 9.6rem;
   }
@@ -21,6 +34,22 @@ export default function Navbar() {
   return (
     <Wrapper as="nav">
       <Logo />
+      <div className="socials">
+        <a
+          href="https://wa.link/o3b1bj"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={whatsapp} alt="instagram" className="icon" />
+        </a>
+        <a
+          href="https://instagram.com/blunchng"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={instagram} alt="instagram" className="icon" />
+        </a>
+      </div>
     </Wrapper>
   );
 }
